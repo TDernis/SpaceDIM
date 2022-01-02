2a76a43c903a98d7b0633d826a393715f90954ce
Thibault (Dinnox) <thibault-dernis@hotmail.fr>
2022-01-02 21:42:13 +0100

    Data binding fix

--- app/src/main/java/fr/thibault/spacedim/LoginFragment.kt
+++ app/src/main/java/fr/thibault/spacedim/LoginFragment.kt
@@ -8,26 +8,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
-
+import fr.thibault.spacedim.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    /*val binding: FragmentLoginBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+            inflater, R.layout.fragment_login, container, false)
 
-        //binding.playButton.setOnClickListener { view : View ->
-        //    view.findNavController().navigate(R.id.action_loginFragment_to_emptyWaitingRoomFragment)
-        //}
+        /*binding.playButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_emptyWaitingRoomFragment)
+        }*/
 
         setHasOptionsMenu(true)
 
         return binding.root
-    }*/
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("LoginFragment", "onAttach called")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("LoginFragment", "onCreate called")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("LoginFragment", "onViewCreated called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("LoginFragment", "onStart called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("LoginFragment", "onResume called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("LoginFragment", "onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("LoginFragment", "onStop called")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("LoginFragment", "onDestroyView called")
+    }
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("LoginFragment", "onDetach called")
+    }
 }
+
--- app/src/main/java/fr/thibault/spacedim/MainActivity.kt
+++ app/src/main/java/fr/thibault/spacedim/MainActivity.kt
@@ -13,4 +13,9 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+    }
 }
