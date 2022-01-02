2be74848528d8d4babad7f1f9a1ec2418de42b98
Thibault (Dinnox) <thibault-dernis@hotmail.fr>
2022-01-02 21:18:20 +0100

    Navigation fix WIP

---
+++ app/src/main/java/fr/thibault/spacedim/LoginFragment.kt
@@ -0,0 +1,33 @@
+package fr.thibault.spacedim
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+
+
+class LoginFragment : Fragment() {
+
+    /*val binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //binding.playButton.setOnClickListener { view : View ->
+        //    view.findNavController().navigate(R.id.action_loginFragment_to_emptyWaitingRoomFragment)
+        //}
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }*/
+}
